9d4e99839d7057e7d8afcc42d6690cc4c5bc4697
SummerFlower <summerflower@SummerFlowerui-MacBook-Pro.local>
2019-11-19 20:33:00 +0900

    struct ctor fix

--- 2048.c
+++ 2048.c
@@ -22,13 +22,33 @@
  */
 
 typedef struct mygame{
-    /*
-    * initialize struct
-    */
-    mygame(){
-        lastAdd = 0;
-        score = 0;
-        win = 0;
+
+    int *(boardLt[SIZE][SIZE]); // left (canonical)
+    int *(boardRt[SIZE][SIZE]); // right
+    int *(boardUp[SIZE][SIZE]); // up
+    int *(boardDn[SIZE][SIZE]); // down
+    int *lastAdd; // address of last tile added
+    int score;
+    int win;
+
+    void (*cleanup)(struct mygame *g);
+    int (*quit)(int op,struct mygame *g);
+    void (*printBoard)(struct mygame *g);
+    void (*addRandom)(struct mygame *g);
+    int (*move)(struct mygame *g);
+    bool (*slide)(int *b[SIZE][SIZE],struct mygame *g);
+    bool (*isFull)(struct mygame *g);
+}mygame;
+
+/*
+* initialize struct
+*/
+
+mygame* initialize(){
+    mygame* g = malloc(sizeof(mygame));
+    g->lastAdd = 0;
+    g->score = 0;
+    g->win = 0;
         for (int r = 0; r < SIZE; r++) { // row for canonical board
             for (int c = 0; c < SIZE; c++) { // column for canonical board
                 int *tile = (int *) malloc(sizeof(int));
@@ -39,40 +59,25 @@ typedef struct mygame{
                 *tile = 0;
 
                 // add tile to equivalent location in all directional boards
-                boardLt[r][c]        = tile;
-                boardRt[r][SIZE-1-c] = boardLt[r][c];
-                boardUp[SIZE-1-c][r] = boardLt[r][c];
-                boardDn[c][SIZE-1-r] = boardLt[r][c];
+                g->boardLt[r][c]        = tile;
+                g->boardRt[r][SIZE-1-c] = g->boardLt[r][c];
+                g->boardUp[SIZE-1-c][r] = g->boardLt[r][c];
+                g->boardDn[c][SIZE-1-r] = g->boardLt[r][c];
             }
         }
-    }
 
-    int *(boardLt[SIZE][SIZE]); // left (canonical)
-    int *(boardRt[SIZE][SIZE]); // right
-    int *(boardUp[SIZE][SIZE]); // up
-    int *(boardDn[SIZE][SIZE]); // down
-    int *lastAdd; // address of last tile added
-    int score;
-    int win;
-
-    void (*cleanup)(struct mygame g);
-    int (*quit)(int op,struct mygame g);
-    void (*printBoard)(struct mygame g);
-    void (*addRandom)(struct mygame g);
-    int (*move)(struct mygame g);
-    bool (*slide)(int *b[SIZE][SIZE],struct mygame g);
-    bool (*isFull)(struct mygame g);
-}mygame;
+    return g;
+}
 
 /*
  * Free boards using boardLt.
  */
 
 void
-cleanup(struct mygame g) {
+cleanup(struct mygame *g) {
     for (int r = 0; r < SIZE; r++) {
         for (int c = 0; c < SIZE; c++) {
-            free(g.boardLt[r][c]);
+            free(g->boardLt[r][c]);
         }
     }
 }
@@ -82,7 +87,7 @@ cleanup(struct mygame g) {
  */
 
 int
-quit(int op,struct mygame g) {
+quit(int op,struct mygame *g) {
     const char *msg = op == 1 ? "YOU WIN!": op == 0 ? "GAME OVER." : "QUIT";
 
     printf("\n\n%s\n", msg);
@@ -105,15 +110,15 @@ terminate(int signum) {
  */
 
 void
-printBoard(struct mygame g) {
-    printf("\n2048\n\nSCORE: %d\n\n", g.score);
+printBoard(struct mygame *g) {
+    printf("\n2048\n\nSCORE: %d\n\n", g->score);
     for (int r = 0; r < SIZE; r++) {
         for (int c = 0; c < SIZE; c++) {
-            if (*g.boardLt[r][c]) {
-                if (g.lastAdd == g.boardLt[r][c])
-                    printf("\033[036m%6d\033[0m", *g.boardLt[r][c]); // with color
+            if (*g->boardLt[r][c]) {
+                if (g->lastAdd == g->boardLt[r][c])
+                    printf("\033[036m%6d\033[0m", *g->boardLt[r][c]); // with color
                 else
-                    printf("%6d", *g.boardLt[r][c]);
+                    printf("%6d", *g->boardLt[r][c]);
             } else {
                 printf("%6s", ".");
             }
@@ -130,15 +135,15 @@ printBoard(struct mygame g) {
  */
 
 void
-addRandom(struct mygame g) {
+addRandom(struct mygame *g) {
     int r, c;
     do {
         r = rand()%4;
         c = rand()%4;
-    } while (*g.boardLt[r][c] != 0);
+    } while (*g->boardLt[r][c] != 0);
     int two_or_four = 2 * rand()%2 + 2;
-    *g.boardLt[r][c] = two_or_four;
-    g.lastAdd = g.boardLt[r][c];
+    *g->boardLt[r][c] = two_or_four;
+    g->lastAdd = g->boardLt[r][c];
 }
 
 /*
@@ -146,7 +151,7 @@ addRandom(struct mygame g) {
  */
 
 bool
-slide(int *b[SIZE][SIZE],struct mygame g) {
+slide(int *b[SIZE][SIZE],struct mygame *g) {
     bool success = 0; // true if something moves
     int marker = -1;  // marks a cell one past location of a previous merge
 
@@ -164,9 +169,9 @@ slide(int *b[SIZE][SIZE],struct mygame g) {
             // merge tile with target tile
             if (newc != c) {
                 if (*b[r][newc]) {
-                    g.score += *b[r][newc] += *b[r][c];
+                    g->score += *b[r][newc] += *b[r][c];
                     marker = newc+1;
-                    if (*b[r][newc] == GOAL) g.win = true;
+                    if (*b[r][newc] == GOAL) g->win = true;
                 } else {
                     *b[r][newc] += *b[r][c];
                 }
@@ -186,22 +191,22 @@ slide(int *b[SIZE][SIZE],struct mygame g) {
  */
 
 int
-move(struct mygame g) {
+move(struct mygame *g) {
     bool success = false;
 
     char direction = getchar();
     switch(direction) {
         case 119:      // 'w' key; up
-            success = slide(g.boardUp,g);
+            success = slide(g->boardUp,g);
             break;
         case 97:       // 'a' key; left
-            success = slide(g.boardLt,g);
+            success = slide(g->boardLt,g);
             break;
         case 115:       // 's' key; down
-            success = slide(g.boardDn,g);
+            success = slide(g->boardDn,g);
             break;
         case 100:       // 'd' key; right
-            success = slide(g.boardRt,g);
+            success = slide(g->boardRt,g);
             break;
         case 113:       // 'q' key; quit
             quit(QUIT,g);
@@ -221,20 +226,20 @@ move(struct mygame g) {
  */
 
 bool
-isFull(struct mygame g) {
+isFull(struct mygame *g) {
     for (int r = SIZE-1; r >= 0; r--) {
         for (int c = SIZE-1; c >= 0; c--) {
 
             // check tile above where there is a row above
             if (r &&
-                (*g.boardLt[r-1][c] == 0 ||
-                 *g.boardLt[r-1][c] == *g.boardLt[r][c]))
+                (*g->boardLt[r-1][c] == 0 ||
+                 *g->boardLt[r-1][c] == *g->boardLt[r][c]))
                 return false;
 
             // check tile to left where there is a column to the left
             if (c &&
-                (*g.boardLt[r][c-1] == 0 ||
-                 *g.boardLt[r][c-1] == *g.boardLt[r][c]))
+                (*g->boardLt[r][c-1] == 0 ||
+                 *g->boardLt[r][c-1] == *g->boardLt[r][c]))
                 return false;
         }
     }
@@ -247,32 +252,32 @@ isFull(struct mygame g) {
 
 int
 main(int argc, char **argv){
-    mygame g;
+    mygame* g=initialize();
 
-    g.cleanup = cleanup;
-    g.quit = quit;
-    g.printBoard = printBoard;
-    g.addRandom = addRandom;
-    g.move = move;
-    g.slide = slide;
-    g.isFull = isFull;
+    g->cleanup = cleanup;
+    g->quit = quit;
+    g->printBoard = printBoard;
+    g->addRandom = addRandom;
+    g->move = move;
+    g->slide = slide;
+    g->isFull = isFull;
 
     srand(time(NULL));         // seed random number
     signal(SIGINT, terminate); // set up signal to handle ctrl-c
     system("stty cbreak");     // read user input immediately
-    g.addRandom(g);
+    g->addRandom(g);
 
     for (;;) {
         printf("\e[1;1H\e[2J"); // clear screen
-        g.addRandom(g);
-        g.printBoard(g);
-        if (g.isFull(g)) break;
-        if (g.win) quit(WIN,g);
-        while(!g.move(g))
+        g->addRandom(g);
+        g->printBoard(g);
+        if (g->isFull(g)) break;
+        if (g->win) quit(WIN,g);
+        while(!g->move(g))
             ;
     }
 
     quit(LOSE,g); // game over
-    g.cleanup(g);  // not reached
+    g->cleanup(g);  // not reached
     return 0;
 }
